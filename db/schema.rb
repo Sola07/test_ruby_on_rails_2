6a784a450a29a0b0ba032840eaedd750dee7682d
Flavie Guiselin <flavie.sola.guiselin@gmail.com>
2024-04-23 11:29:30 +0200

    Add form on home view to upload a csv file

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,71 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_23_092318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.integer "number"
+    t.date "date"
+    t.string "time"
+    t.integer "ticket_number"
+    t.string "sales_channel"
+    t.bigint "event_id", null: false
+    t.bigint "pricing_id", null: false
+    t.bigint "purchaser_id", null: false
+    t.bigint "show_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_bookings_on_event_id"
+    t.index ["pricing_id"], name: "index_bookings_on_pricing_id"
+    t.index ["purchaser_id"], name: "index_bookings_on_purchaser_id"
+    t.index ["show_id"], name: "index_bookings_on_show_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.date "starting_date"
+    t.date "ending_date"
+    t.string "starting_time"
+    t.string "ending_time"
+    t.integer "key"
+    t.bigint "show_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["show_id"], name: "index_events_on_show_id"
+  end
+
+  create_table "pricings", force: :cascade do |t|
+    t.float "amount"
+    t.string "product_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "purchasers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "address"
+    t.integer "zipcode"
+    t.string "country"
+    t.integer "age"
+    t.string "gender"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shows", force: :cascade do |t|
+    t.integer "key"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "bookings", "events"
+  add_foreign_key "bookings", "pricings"
+  add_foreign_key "bookings", "purchasers"
+  add_foreign_key "bookings", "shows"
+  add_foreign_key "events", "shows"
 end
