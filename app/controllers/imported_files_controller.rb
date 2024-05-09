12ca98ee66f60544a76f69aec453f5805a3b4dd8
Flavie Guiselin <flavie.sola.guiselin@gmail.com>
2024-05-09 20:13:37 +0200

    Add an alert for non supported files

--- app/controllers/imported_files_controller.rb
+++ app/controllers/imported_files_controller.rb
@@ -1,14 +1,22 @@
 class ImportedFilesController < ApplicationController
   require_relative "../services/file_import_service.rb"
 
-  def index;end
+  def index; end
 
   def import
     file = params[:file]
-    imported_file = ImportedFile.create(file_name: file.original_filename, file_type: file.original_filename.split(".")[1])
-    service = "#{imported_file.file_type.capitalize}ImportService"
-    service.constantize.call(file, imported_file.id)
-    redirect_to root_path(imported_file_id: imported_file.id), notice: "File imported successfully."
+    file_type = file.original_filename.split(".")[1]
+
+    begin
+      imported_file = ImportedFile.create(file_name: file.original_filename, file_type: file_type)
+
+      service = "#{file_type.capitalize}ImportService"
+      service.constantize.call(file, imported_file.id)
+
+      redirect_to root_path(imported_file_id: imported_file.id), notice: "File imported successfully."
+    rescue NameError
+      redirect_to root_path, alert: "Unsupported file type: #{file_type}"
+    end
   end
 
   def show
